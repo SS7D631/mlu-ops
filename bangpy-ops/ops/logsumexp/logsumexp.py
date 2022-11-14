6ccebd3eaf090d463100e984f613aaac89bc2adb
1422749473@qq.com <testouya>
2022-11-14 23:38:36 +0800

    完成初步，能运行了。

--- bangpy-ops/ops/logsumexp/logsumexp.py
+++ bangpy-ops/ops/logsumexp/logsumexp.py
@@ -28,300 +28,25 @@ import bangpy.eager as eg
 DTYPES = [#bangpy.float16,
            bangpy.float32]
 TARGET_LIST = ["mlu370-s4"]
-KERNEL_NAME = "pairwise_distance"
+KERNEL_NAME = "Logsumexp"
 
 
 @eg.module
-class PairwiseDistance(object):
-    def calc_core_process_count(self, data_total_len: ty.int32, task_num: ty.int32, task_id: ty.int32):
-        one_core_count = data_total_len // task_num
-        remain = data_total_len % task_num
-        m_current_core_start = 0
-        m_current_core_end = 0
-        m_total_count_in_core = 0
-        if task_id < remain:
-            m_current_core_start = (one_core_count + 1) * task_id
-            m_current_core_end = (one_core_count + 1) * (task_id + 1) - 1
-            m_total_count_in_core = m_current_core_end - m_current_core_start + 1
-        else:
-            m_current_core_start = (one_core_count + 1) * \
-                remain + one_core_count * (task_id - remain)
-            m_current_core_end = (one_core_count + 1) * remain + \
-                one_core_count * (task_id - remain) + one_core_count - 1
-            m_total_count_in_core = m_current_core_end - m_current_core_start + 1
-
-        self.m_total_count_in_core = m_total_count_in_core
-        self.m_current_core_start = m_current_core_start
-        self.m_current_core_end = m_current_core_end
-
-    def calc_avaible_nram_count(self):
-        return tcp.round_down((self.bp.nram_size - 30 * 1024) // 2, 128)
-
-    def sub_tensor(self, t1: ty.handle, t2: ty.handle, len_t2: ty.int32):
-#        nram_available_size = tcp.round_down((self.bp.nram_size - 30 * 1024) // 2, 128)
-        nram_available_size = self.calc_avaible_nram_count()
-
-        nram_process_count = nram_available_size // self.dtype_size
-
-        nram_buffer_in = tcp.alloc_buffer(
-            shape=(2, nram_process_count),
-            dtype=self.dtype,
-            scope="nram")
-
-        nram_buffer_in0 = nram_buffer_in[0]
-        nram_buffer_in1 = nram_buffer_in[1]
-
-        total_count_in_core = self.m_total_count_in_core
-        calc_loop_count = (total_count_in_core + nram_process_count - 1) // nram_process_count
-
-        for i in range(calc_loop_count):
-            calc_size = 0
-            if i < calc_loop_count - 1:
-                calc_size = nram_process_count
-            else:
-                calc_size = total_count_in_core % nram_process_count
-                if calc_size == 0:
-                    calc_size = nram_process_count
-
-            once_loop_start = self.m_current_core_start + nram_process_count * i
-
-            # tensor1 copy
-            tcp.memcpy(nram_buffer_in0[0:calc_size], t1[once_loop_start:once_loop_start + calc_size])
-
-            # tensor2 copy
-            head_offset = once_loop_start % len_t2
-
-            head_len = 0
-            if head_offset == 0:
-                head_len = 0
-            else:
-                head_len = len_t2 - head_offset
-
-            if head_len >= calc_size:
-                tcp.memcpy(nram_buffer_in1[0:calc_size], t2[head_offset:head_offset + calc_size])
-            else:
-                if head_len > 0:
-                    tcp.memcpy(nram_buffer_in1[0:len_t2 - head_offset], t2[head_offset:len_t2])
-
-                total_offset = head_len
-                body_cp_count = (calc_size - head_len) // len_t2
-
-                for i in range(body_cp_count):
-                    tcp.memcpy(nram_buffer_in1[total_offset:total_offset + len_t2], t2[0:len_t2])
-                    total_offset = total_offset + len_t2
-
-                offset_end = (once_loop_start + calc_size) % len_t2
-
-                if offset_end > 0:
-                    tcp.memcpy(nram_buffer_in1[total_offset:total_offset + offset_end], t2[0:offset_end])
-
-
-            tcp.subtract(nram_buffer_in0, nram_buffer_in0, nram_buffer_in1)
-            tcp.abs(nram_buffer_in0, nram_buffer_in0)
-
-            # subtract eps
-            eps = -self.gram_paras[1]
-            tcp.add(nram_buffer_in0, nram_buffer_in0, eps)
-
-            tcp.log(nram_buffer_in0, nram_buffer_in0)
-            p = self.gram_paras[0]
-            tcp.multiply(nram_buffer_in0, nram_buffer_in0, p)
-            tcp.exp(nram_buffer_in0, nram_buffer_in0)
-
-            tcp.memcpy(t1[once_loop_start:once_loop_start + calc_size], nram_buffer_in0[:calc_size])
-
-    def copy_from_2d_tensor(self, dst: ty.handle, offset_dst: ty.int32, src: ty.handle, \
-        offset_src: ty.int32, dim_len: ty.int32, width: ty.int32, cp_len: ty.int32):
-        big_row = offset_src // (width * dim_len)
-        m = offset_src % dim_len + big_row * dim_len
-
-        big_n = offset_src % dim_len
-        n = big_n % width
-
-        if offset_dst != offset_dst + cp_len // 2:
-            tcp.memcpy(dst[offset_dst:offset_dst + cp_len // 2, 0:1], \
-                src[m:m + cp_len  // 2, n:n + 1])
-
-        if offset_dst + cp_len // 2 != offset_dst + cp_len:
-            tcp.memcpy(dst[offset_dst + cp_len // 2:offset_dst + cp_len, 0:1], \
-                src[m + cp_len // 2:m + cp_len, n:n + 1])
-
-    def calc_norm(self, buffer: ty.handle, start: ty.int32, end: ty.int32):
-        result = 0.0
-        size = end - start
-        for i in range(size):
-            result += buffer[start + i]
-        return result
-
-    """Operator description:
-    Add the data in the two buffers.
-    """
+class Logsumexp(object):
     def __init__(self, dtype: ty.string, dtype_size: ty.int32) -> None:
         self.dtype = dtype
-        self.dtype_size = dtype_size
-
-    def get_norm_index(self, data_pos: ty.int32, dim_len: ty.int32):
-        index = (data_pos + dim_len - 1) // dim_len
-        return index - 1
-
-    def calc_pairwise_distance1(self, gram_tensor: ty.handle, border_outputs: ty.handle, idx_outputs: ty.handle, outputs: ty.handle):
-        current_core_start = self.m_current_core_start
-        total_count_in_core = self.m_total_count_in_core
-        calc_loop_count = (total_count_in_core + self.nram_process_count - 1) // self.nram_process_count
-        norm_value = 0.0
-
-        once_loop_start = 0
-        oper_type = 0
-        pw = 1 / self.gram_paras[0]
-        dim_len = self.pd_len
-        norm_offset = current_core_start % dim_len
-
-        if norm_offset == 0:
-            oper_type = 2
-        else:
-            oper_type = 0
-
-        flat_nram = self.nram_calc_buffer[:self.nram_process_count].reshape([self.nram_process_count, ])
-        norm_value = 0.0
-        calc_size = self.nram_process_count
-
-        once_norm_ok = 0
-        cp_data_len = 0
-        for i in range(calc_loop_count):
-            once_loop_start = current_core_start + self.nram_process_count * i
-            if i == calc_loop_count - 1:
-                calc_size = total_count_in_core % self.nram_process_count
-                if calc_size == 0:
-                    calc_size = self.nram_process_count
-
-            norm_offset = once_loop_start % dim_len
-            expect_cp_len = dim_len - norm_offset
-
-            if expect_cp_len > calc_size:
-                expect_cp_len = calc_size
-                self.copy_from_2d_tensor(self.nram_calc_buffer, 0, gram_tensor, once_loop_start, \
-                    dim_len, self.pd_width, expect_cp_len)
-                cp_data_len = cp_data_len + expect_cp_len
-                seg_norm_value = self.calc_norm(flat_nram, 0, expect_cp_len)
-                norm_value = norm_value + seg_norm_value
-                if i == calc_loop_count - 1:  # last loop
-                    index = self.get_norm_index(once_loop_start + expect_cp_len, dim_len)
-                    if once_norm_ok == 0:
-                        border_outputs[self.taskId * 2] = norm_value
-                        idx_outputs[self.taskId * 2] = index
-                    else:
-                        border_outputs[self.taskId * 2 + 1] = norm_value
-                        idx_outputs[self.taskId * 2 + 1] = index
-            else:
-                self.copy_from_2d_tensor(self.nram_calc_buffer, 0, gram_tensor, once_loop_start, \
-                    dim_len, self.pd_width, expect_cp_len)
-                cp_data_len = cp_data_len + expect_cp_len
-                seg_norm_value = self.calc_norm(flat_nram, 0, expect_cp_len)
-
-                norm_value = norm_value + seg_norm_value
-
-                once_norm_ok = 1
-                index = self.get_norm_index(once_loop_start + expect_cp_len, dim_len)
-
-                if cp_data_len < dim_len:
-                    border_outputs[self.taskId * 2] \
-                        = norm_value
-                    idx_outputs[self.taskId * 2] = index
-                else:
-                    outputs[index] = tcp.scalar_pow(norm_value, pw)  # norm complete
+        self.dtype_size = dtype_size 
 
 
-                norm_value = 0.0
-                cp_data_len = calc_size - expect_cp_len
-                if cp_data_len > 0:
-                    self.copy_from_2d_tensor(self.nram_calc_buffer, 0, gram_tensor, \
-                        once_loop_start + expect_cp_len, dim_len, \
-                        self.pd_width, cp_data_len)
-                    calc_result = self.calc_norm(flat_nram, 0, cp_data_len)
-
-                    norm_value = calc_result
-                    if i == calc_loop_count - 1:
-                        border_outputs[self.taskId * 2 + 1] = norm_value
-                        idx_outputs[self.taskId * 2 + 1] = index + 1
-
-         
-
-
-    def calc_pairwise_distance2(self, gram_tensor: ty.handle, border_outputs: ty.handle, idx_outputs: ty.handle, outputs: ty.handle):
-        current_core_start = self.m_current_core_start
-        total_count_in_core = self.m_total_count_in_core
-        dim_len = self.pd_len
-        norm_value = 0.0
-        pw = 1 / self.gram_paras[0]
-
-        flat_nram = self.nram_calc_buffer[:self.nram_process_count].reshape([self.nram_process_count, ])
-
-        norm_offset = current_core_start % dim_len
-        expect_cp_len = 0
-
-        if norm_offset != 0:
-            expect_cp_len = dim_len - norm_offset
-            self.copy_from_2d_tensor(self.nram_calc_buffer, 0, gram_tensor, current_core_start, \
-                dim_len, self.pd_width, expect_cp_len)
-            calc_result = self.calc_norm(flat_nram, 0, expect_cp_len)
-            norm_value = calc_result
-            index = self.get_norm_index(current_core_start + expect_cp_len, dim_len)
-            border_outputs[self.taskId * 2] = norm_value
-            idx_outputs[self.taskId * 2] = index
-
-        norm_start_pos = current_core_start + expect_cp_len
-
-        nram_norm_count = self.nram_process_count // dim_len
-
-        total_norm_in_core = (total_count_in_core - expect_cp_len) // dim_len
-
-        calc_loop_count = (total_norm_in_core + nram_norm_count - 1) // nram_norm_count
-
-        once_loop_norm_count = nram_norm_count
-
-        for i in range(calc_loop_count):
-            once_loop_start = norm_start_pos + nram_norm_count * dim_len * i
-            if i == calc_loop_count - 1:
-                once_loop_norm_count = total_norm_in_core % nram_norm_count
-                if once_loop_norm_count == 0:
-                    once_loop_norm_count = nram_norm_count
-
-            start_index = once_loop_start // dim_len
-            for j in range(once_loop_norm_count):
-                self.copy_from_2d_tensor(self.nram_calc_buffer, \
-                    0, gram_tensor, once_loop_start + j * \
-                    dim_len, dim_len, self.pd_width, dim_len)
-                calc_result = self.calc_norm(flat_nram, 0, dim_len)
-                norm_value = calc_result
-                outputs[start_index + j] = tcp.scalar_pow(norm_value, pw)
-
-        norm_loop_end_pos = norm_start_pos + total_norm_in_core * dim_len
-        cur_loop_end_pos = current_core_start + total_count_in_core
-
-        if norm_loop_end_pos < cur_loop_end_pos:
-            self.copy_from_2d_tensor(self.nram_calc_buffer, 0, gram_tensor, norm_loop_end_pos, \
-                dim_len, \
-                self.pd_width, cur_loop_end_pos - norm_loop_end_pos)
-            calc_result = self.calc_norm(flat_nram, 0, cur_loop_end_pos - norm_loop_end_pos)
-            norm_value = calc_result
-            index = self.get_norm_index(norm_loop_end_pos + 1, dim_len)
-            border_outputs[self.taskId * 2 + 1] = norm_value
-            idx_outputs[self.taskId * 2 + 1] = index
-
-    def main(self, Gram_tensor1: ty.handle, Gram_tensor2: ty.handle, Gram_paras: ty.handle,
-                    len_tensor1: ty.int32, len_tensor2: ty.int32,
-                    pd_len: ty.int32, pd_height: ty.int32, pd_width: ty.int32,
+    def main(self, Gram_tensor: ty.handle,
+                    dim_len: ty.int32, h: ty.int32, w: ty.int32,
                     output_len: ty.int32,
-                    Gram_border_buf_out: ty.handle,
-                    Gram_border_idx_out: ty.handle,
-                    Gram_buffer_out: ty.handle
+                    Gram_border_buf_out: ty.handle, Gram_border_idx_out: ty.handle, Gram_buffer_out: ty.handle
                     ) -> None:
         tgt = tcp.target()
         self.bp = tgt
 
-        gram_tensor1 = tcp.match_buffer(Gram_tensor1, [len_tensor1], dtype=self.dtype)
-        gram_tensor2 = tcp.match_buffer(Gram_tensor2, [len_tensor2], dtype=self.dtype)
-        self.gram_paras = tcp.match_buffer(Gram_paras, [2], dtype=self.dtype)
+        gram_tensor = tcp.match_buffer(Gram_tensor, [h * w], dtype=self.dtype)
 
         border_array_size = 128
         gram_border_buf_out = tcp.match_buffer(Gram_border_buf_out, [border_array_size * 2], dtype=self.dtype)
@@ -333,61 +58,15 @@ class PairwiseDistance(object):
                 task_num = tgt.cluster_num * tgt.core_num
                 task_id = tgt.core_num * cluster_id + core_id
                 self.taskId = task_id
+                tcp.print("zouni")
 
-                self.calc_core_process_count(len_tensor1, task_num, task_id)
-                self.sub_tensor(gram_tensor1, gram_tensor2, len_tensor2)
-                tcp.sync_all()
 
-                self.pd_width = pd_width
-                #    tcp.print(len_tensor1, pd_height, pd_width)
-                gram_reshape_tensor = gram_tensor1[:pd_height * pd_width].reshape([pd_height, pd_width])
-                    #gram_reshape_tensor = gram_tensor1[:len_tensor1].reshape([pd_height, pd_width])
-                tcp.sync_all()
-
-                nram_available_size = self.calc_avaible_nram_count()
-                self.nram_process_count = nram_available_size // self.dtype_size
-
-                self.nram_calc_buffer = tcp.alloc_buffer(
-                    shape=(self.nram_process_count, 1),
-                    dtype=self.dtype,
-                    scope="nram")
-
-                self.pd_len = pd_len
-
-                if self.pd_len > self.nram_process_count:
-                    self.calc_pairwise_distance1(gram_reshape_tensor, gram_border_buf_out, \
-                        gram_border_idx_out, gram_buffer_out)
-                else:
-                    if len_tensor1 // task_num + 1 < self.pd_len:
-                        self.calc_pairwise_distance1(gram_reshape_tensor, \
-                            gram_border_buf_out, gram_border_idx_out, gram_buffer_out)
-                    else:
-                        self.calc_pairwise_distance2(gram_reshape_tensor, gram_border_buf_out, \
-                            gram_border_idx_out, gram_buffer_out)
-
-                tcp.sync_all()
-
-                #tcp.print(gram_border_buf_out)
-                #tcp.print(gram_border_idx_out)
-
-                if task_id == 0:
-                    for i in range(border_array_size):
-                        index1 = gram_border_idx_out[2 * i]
-                        index2 = gram_border_idx_out[2 * i + 1]
-                        norm_value1 = gram_border_buf_out[2 * i]
-                        norm_value2 = gram_border_buf_out[2 * i + 1]
-
-                        if index1 >= 0:
-                            gram_buffer_out[index1] = gram_buffer_out[index1] + norm_value1
-
-                        if index2 >= 0:
-                            gram_buffer_out[index2] = gram_buffer_out[index2] + norm_value2
 
 
 @tcp.register_mlu_op(DTYPES, TARGET_LIST, KERNEL_NAME)
 def build_add(dtype=None, target=None):
     f = build_module.build(
-        PairwiseDistance(dtype.name, dtype.bytes), target_tag=target, name=KERNEL_NAME
+        Logsumexp(dtype.name, dtype.bytes), target_tag=target, name=KERNEL_NAME
     )
     return f
 
--- bangpy-ops/ops/logsumexp/test_logsumexp.py
+++ bangpy-ops/ops/logsumexp/test_logsumexp.py
@@ -1,4 +1,4 @@
-# Copyright (C) [2021] by Cambricon, Inc.
+# Copyright (C) [2022] by Cambricon, Inc.
 #
 # Permission is hereby granted, free of charge, to any person obtaining a
 # copy of this software and associated documentation files (the
@@ -20,207 +20,145 @@
 # SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 # pylint: disable=missing-docstring, invalid-name, too-many-locals
 """A multi-platform code link example test for BANGPy TCP."""
+
 import numpy as np
-import math
 import torch
 import pytest
+
 import bangpy as bp
 from bangpy.common import load_op_by_type
-from pairwise_distance import DTYPES, KERNEL_NAME, TARGET_LIST, PairwiseDistance
-import logging
+from logsumexp import DTYPES, KERNEL_NAME, TARGET_LIST, Logsumexp
+
 
 def create_random_shape(length, size):
     return np.random.randint(low=1, high=size, size=length)
 
-ranshp = create_random_shape(2, 512)
+
+ranshp = create_random_shape(2, 10)
 ranshp1 = create_random_shape(5, 3)
 ranshp2 = create_random_shape(10, 2)
+
 
 @pytest.mark.parametrize(
     "shape",
     [
-    [(1, 2, 10241 * 100 ), (1, 2, 10241 * 100)],
-    [(2, 1, 1, 1, 3, 2, 2, 3, 1, 2, 5, 4 ), (5, 4,)],
-    [(30000, 1), (30000, 1)],
-    [(1, 3), (1, 3)],
-    [(4,5,2), (234)],
-    [(1, 482 * 1024), (1, 482 * 1024)],
-    [(32, 482 * 1024), (32, 482 * 1024)],
-    [ranshp, ranshp],
-    [ranshp1, ranshp1],
-    [ranshp2, ranshp2],
-        [[112, 2], [2]],
-        [[112, 12], [1]]
+
+        (3, 1, 2),
+
     ],
 )
-
 @pytest.mark.parametrize(
     "dtype", DTYPES,
 )
-
 @pytest.mark.parametrize(
-    "p", [1, 2.2, 3.5, -1.2],
+    "dim", [2],
 )
-
 @pytest.mark.parametrize(
-    "eps", [0.000001, 0.0001],
+    "keepdim", [True],
 )
-
-@pytest.mark.parametrize(
-    "keepdim", [False, True],
-)
-
-
-def test_pairwise_distance(target, shape, dtype, p, eps, keepdim):    
+def test_logsumexp(target, shape, dim, dtype, keepdim):
     if target not in TARGET_LIST:
         return
 
-    def mlu_pairwise_distance(p=2.0, eps=1e-06, keepdim=False):
+    try:
+        total_input_len = 1
+        for s in shape:
+            total_input_len *= s
+
+        input_tensor = np.random.uniform(low=-5, high=5, size=shape).astype(dtype.as_numpy_dtype)
+
         def get_total_size(shp):
             size = 1
-            for s in shp:
-                size *= s
+            for each_size in shp:
+                size *= each_size
             return size
 
-        def check_shape(s1, s2):
-            if len(s2) == 1:
-                if s2[0] == 1:
-                    return True
-
-            offset = len(s1) - len(s2)
-            i = 0
-            for _ in s2:
-                if s1[offset + i] != s2[i]:
-                    return False
-                i += 1
+        def check_dim_range(dim_num, shape_param):
+            if dim_num < 0 or dim_num >= len(shape_param):
+                return False
             return True
 
-        def f(a, b):
-            if len(a) == 0 or len(b) == 0:
-                raise Exception("shape err")
+        _dev = bp.device(0)
+        shp_len = len(shape)
 
-            if len(a.shape) == 1 and len(b.shape) == 1:
-                raise Exception("shape err")
+        if dim < 0:
+            dim += len(shape)
 
-            #拿到shape
-            if len(a.shape) > len(b.shape):
-                _shape1 = a.shape
-                _shape2 = b.shape
+        if not check_dim_range(dim, shape):
+            raise Exception('dim err')
+
+        _pd_len = shape[dim]
+        _pd_height = 1
+        _pd_width = 1
+
+        for i in range(0, dim + 1):
+            _pd_height *= shape[i]
+
+        if dim == shp_len - 1:
+            pass
+        else:
+            for i in range(dim + 1, shp_len):
+                _pd_width *= shape[i]
+
+        _mlu_input1 = bp.Array(input_tensor.flatten(), _dev)
+
+        _output_len = get_total_size(shape) // shape[dim]
+        output_buffer = -np.ones(_output_len, dtype=dtype.as_numpy_dtype)
+        _mlu_output = bp.Array(output_buffer, _dev)
+
+        output_count = 256
+        output_buffer2 = np.zeros(output_count, dtype=dtype.as_numpy_dtype)
+        _mlu_border_output = bp.Array(output_buffer2, _dev)
+
+        output_buffer3 = -np.ones(output_count, dtype=np.int32)
+        _mlu_border_idx_output = bp.Array(output_buffer3, _dev)
+
+        eager_mode = True
+        if not eager_mode:
+            func = load_op_by_type(KERNEL_NAME, dtype.name)
+            func(_mlu_input1,
+                 _pd_len, _pd_height, _pd_width, _output_len,
+                 _mlu_border_output, _mlu_border_idx_output, _mlu_output)
+            result = _mlu_output.numpy()
+        else:
+            func = Logsumexp(dtype.name, dtype.bytes)
+            func(input_tensor.flatten(),
+             _pd_len, _pd_height, _pd_width, _output_len,
+             output_buffer2, output_buffer3, output_buffer
+            )
+            result = output_buffer
+        
+
+        def create_reshape(is_keepdim, dim_num, shape_param):
+            out_put_shape = []
+            if is_keepdim:
+                for item in shape_param:
+                    out_put_shape.append(item)
+                out_put_shape[dim_num] = 1
             else:
-                _shape1 = b.shape
-                _shape2 = a.shape
+                index = 0
+                for each_size in shape_param:
+                    if index == dim_num:
+                        index += 1
+                        continue
+                    out_put_shape.append(each_size)
+                    index += 1
+            return out_put_shape
 
-            if not check_shape(_shape1, _shape2):
-                raise Exception("shape err")
+        outputshape = create_reshape(keepdim, dim, shape)
 
+        mlu_ret = result.reshape(outputshape)
 
-            _dev = bp.device(0)
+        x = torch.Tensor(input_tensor)
+        cpu_ret = torch.logsumexp(x, dim, keepdim)
 
-            dim_index = len(_shape1) - 1
+        #bp.assert_allclose(cpu_ret.numpy(), mlu_ret, rtol=0.01, atol=0.01)
 
-            # mlu input parameters
-            _pd_len = _shape1[len(_shape1) - 1]
-            _pd_height = 1
-            _pd_width = 1
-
-            for i in range(0, dim_index + 1):
-                _pd_height *= _shape1[i]
-
-            if dim_index == len(_shape1) - 1:
-                pass
-            else:
-                for i in range(dim_index + 1, len(_shape1)):
-                    _pd_width *= _shape1[i]
-
-            # mlu input
-            _mlu_input1 = bp.Array(a.flatten(), _dev)
-            _mlu_input2 = bp.Array(b.flatten(), _dev)
-            paras = np.array([p, eps]).astype(dtype.as_numpy_dtype)
-            _mlu_paras = bp.Array(paras, _dev)
-
-            # mlu output
-            _output_len = get_total_size(_shape1) // _shape1[dim_index]
-            output_buffer = np.zeros(_output_len, dtype=dtype.as_numpy_dtype)
-            _mlu_output = bp.Array(output_buffer, _dev)
-
-            output_count = 256
-            output_buffer2 = np.zeros(output_count, dtype=dtype.as_numpy_dtype)
-            _mlu_border_output = bp.Array(output_buffer2, _dev)
-
-            output_buffer3 = -np.ones(output_count, dtype=np.int32)
-            _mlu_border_idx_output = bp.Array(output_buffer3, _dev)
-
-
-            # call mlu interface
-
-            eager_mode = True
-            if not eager_mode:
-                func = load_op_by_type(KERNEL_NAME, dtype.name, dtype.bytes)
-                func(_mlu_input1, _mlu_input2,
-                     _mlu_paras,
-                     get_total_size(_shape1), get_total_size(_shape2),
-                     _pd_len, _pd_height, _pd_width, _output_len
-                     , _mlu_border_output
-                     , _mlu_border_idx_output
-                     , _mlu_output
-                     )
-                result = _mlu_output.numpy()
-                result_border_idx = _mlu_border_idx_output.numpy()
-            else:
-                func = PairwiseDistance(dtype.name, dtype.bytes)
-                func(a.flatten(), b.flatten(),
-                     paras,
-                     get_total_size(_shape1), get_total_size(_shape2),
-                     _pd_len, _pd_height, _pd_width, _output_len
-                     , output_buffer2
-                     , output_buffer3
-                     , output_buffer
-                     )
-
-                result = output_buffer
-                result_border_idx = output_buffer3
-
-            s = set()
-            for i in result_border_idx:
-                s.add(i)
-
-            for item in s:
-                if item >= 0:
-                    result[item] = math.pow(result[item], 1 / p)
-
-
-            def create_output_shape(shp, dim_idx):
-                outputshape = []
-                if keepdim:
-                    for item in shp:
-                        outputshape.append(item)
-                    outputshape[dim_idx] = 1
-                else:
-                    for i in range(0, len(shp) - 1):
-                        outputshape.append(shp[i])
-                return outputshape
-
-            return result.reshape(create_output_shape(_shape1, dim_index))
-
-        return f
-
-
-    m_ori_input1 = np.random.uniform(low=-5, high=5, size=shape[0])
-    m_ori_input2 = np.random.uniform(low=-5, high=5, size=shape[1])
-
-    try:
-        mlu_ret = mlu_pairwise_distance(p=p, eps=eps, keepdim=keepdim)\
-            (m_ori_input1.astype(dtype.as_numpy_dtype), \
-            m_ori_input2.astype(dtype.as_numpy_dtype))
     except Exception as err:
-        print(str(err))
-        if str(err) == "shape err":
+        strerr = str(err)
+        print(strerr)
+
+        if strerr == "dim err":
             return
 
-        raise Exception(str(err)) from err
-
-
-    cpu_ret = torch.nn.PairwiseDistance(p=p, eps=eps, keepdim=keepdim)\
-        (torch.Tensor(m_ori_input1), torch.Tensor(m_ori_input2)).numpy()
-
-    bp.assert_allclose(cpu_ret, mlu_ret, rtol = 0.01, atol = 0.01)
+        raise Exception(strerr) from err
